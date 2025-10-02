aa09daa7fdb0294d2bb9b9724865bf0b28d17b0d
gargiagnihotri <gargitrader3@gmail.com>
2025-10-02 23:37:21 +0530

    Day10.2 SpiralTraversal

---
+++ SpiralTraversal.java
@@ -0,0 +1,53 @@
+
+public class SpiralTraversal {
+    public static void spiralOrder(int[][] matrix) {
+        if (matrix.length == 0) return;
+
+        int top = 0;
+        int bottom = matrix.length - 1;
+        int left = 0;
+        int right = matrix[0].length - 1;
+
+        while (top <= bottom && left <= right) {
+            
+            for (int i = left; i <= right; i++) {
+                System.out.print(matrix[top][i] + " ");
+            }
+            top++;
+
+            
+            for (int i = top; i <= bottom; i++) {
+                System.out.print(matrix[i][right] + " ");
+            }
+            right--;
+
+        
+            if (top <= bottom) {
+                for (int i = right; i >= left; i--) {
+                    System.out.print(matrix[bottom][i] + " ");
+                }
+                bottom--;
+            }
+
+            
+            if (left <= right) {
+                for (int i = bottom; i >= top; i--) {
+                    System.out.print(matrix[i][left] + " ");
+                }
+                left++;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[][] arr = {
+            {11,12,13,14},
+            {15,16,17,18},
+            {19,20,21,22},
+            {23,24,25,26}
+        };
+
+        System.out.println("Spiral Traversal:");
+        spiralOrder(arr);
+    }
+}

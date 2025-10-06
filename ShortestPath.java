1d95d95d5cf8052c89616e6fe05355e809a2d339
gargiagnihotri <gargitrader3@gmail.com>
2025-10-06 23:58:11 +0530

    Day14.1 ShortestPath

---
+++ ShortestPath.java
@@ -0,0 +1,27 @@
+public class ShortestPath{
+    public static float findPath(String path){
+    int x=0,y=0;
+    for(int i =0;i<path.length();i++){
+        char dir = path.charAt(i);
+        if(dir=='E'){
+            x++;
+        }
+        else if(dir=='W'){
+            x--;
+        }
+        else if(dir=='N'){
+            y++;
+        }
+         else{
+            y--;
+        }
+    }
+    int X2 = x*x;                                         
+    int Y2 =y*y;
+    return (float)Math.sqrt(X2 + Y2);                        
+    }
+    public static void main(String[] args){
+        String path = "WNEENESENNN";
+        System.out.println("the shortest path is: " + findPath(path));
+    }
+}
